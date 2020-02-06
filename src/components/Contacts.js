b2470757ec9364f45ceecbd04f1fbfd6ca692af3
Lewis <malewis5@cba.ua.edu>
2020-02-06 12:27:47 -0600

    Added contacts w/state

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Contact from "./Contact";
 
 class Contacts extends Component {
   constructor() {
@@ -10,13 +11,13 @@ class Contacts extends Component {
           name: "John Doe",
           email: "jdoe@gmail.com",
           phone: "555-555-5555"
-        }
+        },
         {
           id: 2,
           name: "Karen Williams",
           email: "karen@gmail.com",
           phone: "222-222-2222"
-        }
+        },
         {
           id: 3,
           name: "Henry Johnson",
@@ -27,7 +28,20 @@ class Contacts extends Component {
     };
   }
   render() {
-    return <div></div>;
+    const { contacts } = this.state;
+
+    return (
+      <div>
+        {contacts.map(contact => (
+          <Contact
+            name={contact.name}
+            email={contact.email}
+            phone={contact.phone}
+            key={contact.id}
+          />
+        ))}
+      </div>
+    );
   }
 }
 
