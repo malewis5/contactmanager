8e27c8559371ddaf589374e812d86abfb01a9c13
Lewis <malewis5@cba.ua.edu>
2020-02-06 11:59:27 -0600

    Pre Vid 19 CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./App.css";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello, World</h1>
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header branding="Contact Manager" />
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+        <Contact
+          name="Karen Smith"
+          email="karen@gmail.com"
+          phone="333-333-3333"
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
