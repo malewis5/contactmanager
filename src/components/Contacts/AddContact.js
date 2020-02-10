3c97ba10d68da448df1d160773fa5d2fe43000db
Lewis <malewis5@cba.ua.edu>
2020-02-10 16:48:57 -0600

    Axios

--- src/components/Contacts/AddContact.js
+++ src/components/Contacts/AddContact.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Consumer } from "../../context";
-import uuid from "uuid";
 import TextInputGroup from "../Layout/TextInputGroup";
+import axios from "axios";
 
 class AddContact extends Component {
   state = {
@@ -49,13 +49,17 @@ class AddContact extends Component {
     }
 
     const newContact = {
-      id: uuid(),
       name,
       email,
       phone
     };
 
-    dispatch({ type: "ADD_CONTACT", payload: newContact });
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", newContact)
+      .then(response =>
+        dispatch({ type: "ADD_CONTACT", payload: response.data })
+      );
+
     // Clear State
     this.setState({
       name: "",
