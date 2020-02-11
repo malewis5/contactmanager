d8edea7f4ed6970598827d66f0c435159dc64a4d
Lewis <malewis5@cba.ua.edu>
2020-02-11 08:48:06 -0600

    Updates

---
+++ precache-manifest.329bf24b52f52f3afaa2c250a313ffad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07f648e23baeb4ac2c71d73e2105185",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "7375740b5f83accc46df",
+    "url": "/contactmanager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8c675aa72c049eae548e",
+    "url": "/contactmanager/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "7375740b5f83accc46df",
+    "url": "/contactmanager/static/js/2.b3aa5257.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.b3aa5257.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c675aa72c049eae548e",
+    "url": "/contactmanager/static/js/main.2487eda7.chunk.js"
+  },
+  {
+    "revision": "6762a8595d24ce18a2d7",
+    "url": "/contactmanager/static/js/runtime-main.a2c60360.js"
+  }
+]);
